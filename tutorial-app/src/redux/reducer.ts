39f6b0aba4394b060c878fcc881323a5d30aff6a
mosa <mosa@mosanoMacBook-Pro.local>
2021-11-15 16:56:54 +0900

    Redux Action,Reducer,Store一旦エラーは無くなったもの

---
+++ tutorial-app/src/redux/reducer.ts
@@ -0,0 +1,28 @@
+// reducer...Actionから、新たなStateに書き換える
+import { ActionType,  } from './types';
+
+const initState: ActionType = {
+  type: 'CURRENT_GAMESTATE',
+  payload: {
+    turnNum: 1,
+    currentMap: Array(3)
+      .fill('')
+      .map(() => Array(3).fill('')),
+  },
+};
+
+// TODO:state actionの型付け
+export const gameReducer = (state=initState, action: ActionType):ActionType => {
+  switch (action.type) {
+    case 'CURRENT_GAMESTATE':
+      return {
+        type: action.type,
+        payload: {
+          turnNum: action.payload.turnNum,
+          currentMap: action.payload.currentMap,
+        },
+      };
+    default:
+      return state;
+  }
+};
